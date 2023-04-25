380bc8f9a9e709e9182578729a3b46e648af6fcc
roger <fc_roger@163.com>
2023-04-25 11:00:45 +0800

    First commit.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="title">Hello Vue!</div>
+</template>
+
+<style scoped>
+.title{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: chocolate;
+  font-size: 30px;
+}
+</style>
